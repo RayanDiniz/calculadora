920427e9e1e8595b0fbc8589bf1a15b94597190d
rayandiniz <rayancassiokiol@gmail.com>
2022-10-13 20:27:21 -0300

    calculadora

--- App.js
+++ App.js
@@ -1,163 +1,49 @@
-import React from 'react';
-import { Text, View, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
+const outputDisplay = document.getElementById("tela");
+const decimal = document.getElementById("dec");
 
-import styles from './styles';
-
-function Separator() {
-  return <View style={styles.separator} />;
+function display(eId) {
+  if(outputDisplay.value == '0' || outputDisplay.value == 'Syntax error') {
+    outputDisplay.value = eId.textContent;
+  } else {
+    outputDisplay.value += eId.textContent;
+  }
 }
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.title}>Calculadora</Text>
-        <TextInput
-        style={styles.tela}
-        /*onChangeText='ss'
-        value='dd'*/
-      />
-      </View>
-      <Separator />
-      <View style={styles.numeros}>
+function tcClear() {
+  outputDisplay.value = '0';
+  decimal.disabled = false;
+}
 
-        
-      <View style={styles.ladolado}>
-          <TouchableOpacity
-            style={styles.numero}
-            onPress={(Text) => TextInput.this('1')}
-          >
-            <Text style={styles.btntxt}>%</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.numero}
-            onPress={() => Alert.alert('Button with adjusted color pressed')}
-          >
-            <Text style={styles.btntxt}>CE</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.numero}
-            onPress={() => Alert.alert('Button with adjusted color pressed')}
-          >
-            <Text style={styles.btntxt}>C</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.numero}
-            onPress={() => Alert.alert('Button with adjusted color pressed')}
-          >
-            <Text style={styles.btntxt}>+</Text>
-          </TouchableOpacity>
-        </View>
+function backSpace() {
+  if(outputDisplay.value.length == 1 || outputDisplay.value == '0' || outputDisplay.value == 'Syntax error') {
+    outputDisplay.value = '0';
+    decimal.disabled = false;
+  } else {
+    outputDisplay.value = outputDisplay.value.substring(0, outputDisplay.value.length - 1);
+  }
+}
 
-        <View style={styles.ladolado}>
-          <TouchableOpacity
-            style={styles.numero}
-            onPress={(Text) => TextInput.this('1')}
-          >
-            <Text style={styles.btntxt}>1</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.numero}
-            onPress={() => Alert.alert('Button with adjusted color pressed')}
-          >
-            <Text style={styles.btntxt}>2</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.numero}
-            onPress={() => Alert.alert('Button with adjusted color pressed')}
-          >
-            <Text style={styles.btntxt}>3</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.numero}
-            onPress={() => Alert.alert('Button with adjusted color pressed')}
-          >
-            <Text style={styles.btntxt}>-</Text>
-          </TouchableOpacity>
-        </View>
-        
-        <View style={styles.ladolado}>
-          <TouchableOpacity
-            style={styles.numero}
-            onPress={(Text) => TextInput.this('1')}
-          >
-            <Text style={styles.btntxt}>4</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.numero}
-            onPress={() => Alert.alert('Button with adjusted color pressed')}
-          >
-            <Text style={styles.btntxt}>5</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.numero}
-            onPress={() => Alert.alert('Button with adjusted color pressed')}
-          >
-            <Text style={styles.btntxt}>6</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.numero}
-            onPress={() => Alert.alert('Button with adjusted color pressed')}
-          >
-            <Text style={styles.btntxt}>*</Text>
-          </TouchableOpacity>
-        </View>
-        
-        <View style={styles.ladolado}>
-          <TouchableOpacity
-            style={styles.numero}
-            onPress={(Text) => TextInput.this('1')}
-          >
-            <Text style={styles.btntxt}>7</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.numero}
-            onPress={() => Alert.alert('Button with adjusted color pressed')}
-          >
-            <Text style={styles.btntxt}>8</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.numero}
-            onPress={() => Alert.alert('Button with adjusted color pressed')}
-          >
-            <Text style={styles.btntxt}>9</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.numero}
-            onPress={() => Alert.alert('Button with adjusted color pressed')}
-          >
-            <Text style={styles.btntxt}>/</Text>
-          </TouchableOpacity>
-        </View>
-        
-        <View style={styles.ladolado}>
-          <TouchableOpacity
-            style={styles.numero}
-            onPress={(Text) => TextInput.this('1')}
-          >
-            <Text style={styles.btntxt}>X</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.numero}
-            onPress={() => Alert.alert('Button with adjusted color pressed')}
-          >
-            <Text style={styles.btntxt}>0</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.numero}
-            onPress={() => Alert.alert('Button with adjusted color pressed')}
-          >
-            <Text style={styles.btntxt}>,</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.numero}
-            onPress={() => Alert.alert('Button with adjusted color pressed')}
-          >
-            <Text style={styles.btntxt}>=</Text>
-          </TouchableOpacity>
-        </View>
+function setOperator(eId) {
+  if (outputDisplay.value == '0' || outputDisplay.value == 'Syntax error') {
+    outputDisplay.value = '0';
+  } else {
+    outputDisplay.value += eId.textContent;
+    decimal.disabled = false;
+  }
+}
 
-      </View>
-    </SafeAreaView>
-  );
+function setDecimal(eId) {
+  outputDisplay.value += eId.textContent;
+  decimal.disabled = status;
+}
+
+function calculate() {
+  try {
+    if (outputDisplay.value != '') {
+      const calculateResult = eval(outputDisplay.value);
+      outputDisplay.value = calculateResult;
+    }
+  } catch (err) {
+    outputDisplay.value = 'Syntax error';
+  }
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# calculadora
-Aplicativo de calculadora com react-native
+# Calculadora JS
+Aplicativo de calculadora feito com JavaScript, HTML e CSS.
+
+## Execução
+Para executar o projeto, basta rodar o "index.html" no navegador.
+
+## Ajude-me
+Paara fazer uma doação pelo meu trabalho, você pode enviar um pix pelo link https://livepix.gg/rayandiniz
